89027324bac0799212d526d45d22383461646684
maru-chan05 <Josedp_05@hotmail.com>
2024-12-15 16:20:16 -0600

    Primer commit del portafolio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Activa el modo oscuro usando la clase 'dark'
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
